175b5ba1f2d0b270361d2a9519eb3cec7cd45b27
juli0mendes <jmendes@live.com>
2020-12-17 21:33:27 -0300

    chore: added html page with js and css to renderize bucket list

---
+++ src/main/resources/static/js/app.js
@@ -0,0 +1,37 @@
+const appEl = document.getElementById('app');
+const baseUrl = 'http://localhost:8080';
+
+async function hashHandler() {
+  console.log('The hash has changed!', location.hash);
+  const hash = !location.hash ? '#/' : location.hash;
+  appEl.innerHTML = await routes[hash]();
+}
+
+async function init() {
+  const hash = !location.hash ? '#/' : location.hash;
+  appEl.innerHTML = await routes[hash]();
+}
+
+function buildTemplate(tmpId, context) {
+  var template = $('#' + tmpId).html();
+
+  // Compile the template data into a function
+  var templateScript = Handlebars.compile(template);
+  var html = templateScript(context);
+  return html;
+}
+
+const routes = {
+  '#/': () => {
+    return 'default page'
+  },
+  '#/buckets': async() => {
+    const res = await fetch(`${baseUrl}/v1/buckets`)
+    const json = await res.json();
+    return buildTemplate('buckets-list', { buckets: json })
+  }
+}
+
+init();
+
+window.addEventListener('hashchange', hashHandler, false);
